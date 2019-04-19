eb5d88f02b002f5e9cdae93ce17c727c204c7270
hamarkovic <hannah.markovic@gmail.com>
2019-04-19 11:24:10 -0700

    Homework 3

---
+++ morse_code_script_HM1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This script translates characters to morse code.
+# It outputs "The Morse Code for the input word is" followed by the code for each inputted letter on a new line.
+# It requires an input of the "morse.txt" file and of an alpha numeric with spaces between each character.
+# example usage: $ sh morse_code_script_HM1.sh morse.txt E M I L Y
+
+echo "The Morse Code for the input word is"
+for character in "$@"
+do
+	if [ $character != $1 ]
+	then 
+		grep -m 1 $character $1
+	fi
+done
